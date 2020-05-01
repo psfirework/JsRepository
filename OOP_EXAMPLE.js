b5dba222d19049b9ebee72bda3739cb1f752ff33
Svetlana <you@example.com>
2020-05-01 21:39:13 +0300

    test commit

---
+++ OOP_EXAMPLE.js
@@ -0,0 +1,77 @@
+const FunctionalCounter = (initialValue = 0) => {
+    let value = initialValue;
+
+    const increment = () => {
+        value++;
+    };
+
+    const decrement = () => {
+        value--;
+    };
+
+    const getValue = () => {
+        return value;
+    };
+
+    const setValue = (valueToSet) => {
+        value = valueToSet;
+    };
+
+    return {
+        setValue,
+        getValue,
+        decrement,
+        increment,
+    }
+};
+
+const counter = FunctionalCounter();
+
+counter.increment();
+counter.increment();
+
+console.log(counter.getValue());
+
+function ES5Counter(counter = 0) {
+    this.counter = counter;
+}
+
+ES5Counter.prototype.increment = function () {
+    this.counter++;
+};
+
+ES5Counter.prototype.getCounter = function () {
+    return this.counter;
+};
+
+ES5Counter.prototype.decrement = function () {
+    this.counter--;
+};
+
+const counter2 = new ES5Counter(12);
+
+console.log(counter2.getCounter());
+
+class ES6Counter{
+     counter = 0;
+
+    increment() {
+        this.counter++;
+    }
+
+    decrement() {
+        this.counter--;
+    }
+
+    getCounter() {
+        return this.counter;
+    }
+}
+
+const es6Counter = new ES6Counter();
+
+
+console.log(es6Counter.getCounter());
+
+
+
